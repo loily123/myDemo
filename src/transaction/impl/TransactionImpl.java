9c2efed8b78b95eb7aa029d757759d1a67cf3b0c
loily123 <2810637851@qq.com>
2019-05-09 15:37:13 +0800

    代码优化

--- src/transaction/impl/TransactionImpl.java
+++ src/transaction/impl/TransactionImpl.java
@@ -1,33 +1,31 @@
 package transaction.impl;
 
 import java.sql.*;
-
 import transaction.Transaction;
 import util.JDBCUtil;
 
 public class TransactionImpl implements Transaction {
-
 	@Override
 	public void startTransaction() throws SQLException {
 		// TODO Auto-generated method stub
-		Connection connection = JDBCUtil.getConnection();
-		connection.setAutoCommit(false);
+		Connection conn=JDBCUtil.getConnection();
+		conn.setAutoCommit(false);
 	}
 
 	@Override
 	public void commit() throws SQLException {
 		// TODO Auto-generated method stub
-		Connection connection = JDBCUtil.getConnection();
-		connection.commit();
-		JDBCUtil.close(null, null, connection);
+		Connection conn=JDBCUtil.getConnection();
+		conn.commit();
+		JDBCUtil.close(null, null, conn);
 	}
 
 	@Override
 	public void rollback() throws SQLException {
 		// TODO Auto-generated method stub
-		Connection connection = JDBCUtil.getConnection();
-		connection.rollback();
-		JDBCUtil.close(null, null, connection);
+		Connection conn=JDBCUtil.getConnection();
+		conn.rollback();
+		JDBCUtil.close(null, null, conn);
 	}
 
 }
